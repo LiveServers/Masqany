b3812ca366123bb70426406bf26b6f1a45c28df8
Brian Kyole <briankyole@wowzi.co>
2025-01-19 22:57:48 +0300

    chore: setup remix template

---
+++ apps/masqany-web/app/root.tsx
@@ -0,0 +1,46 @@
+import {Links, Meta, Outlet, Scripts, ScrollRestoration} from "@remix-run/react"
+import type { MetaFunction } from "@remix-run/react"
+import stylesheet from "./tailwind.css";
+import type { LinksFunction } from "@remix-run/node";
+import { ReactNode } from "react";
+
+export const meta: MetaFunction = () => {
+    return [{
+        title: 'Masqany Rental Property Management'
+    }]
+}
+export const link: LinksFunction = () => [
+    {rel: 'stylesheet', href: stylesheet},
+    {rel: 'icon', href: '/favicon-masqany.png', type: 'image/png'}
+]
+
+export function Layout({children}:{children: ReactNode}){
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    )
+}
+
+export default function App() {
+    return <Outlet />
+}
+
+export function ErrorBoundary() {
+    return (
+        <>
+        <link rel="stylesheet" type="text/css" href={stylesheet} />
+        <h1>An error occured</h1>
+        </>
+    )
+}
